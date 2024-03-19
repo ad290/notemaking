dc934e3e7c3083cd6fc4acb548d31f9772b4d2e0
Aditya Pawar <adityapawar02468@gmail.com>
2024-03-19 20:05:31 +0530

    changes are made then again uploaded

---
+++ src/components/AddNote.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function AddNote({
+  handleNoteText,
+  handleAddNote,
+  handleDownloadNote,
+  noteText,
+}) {
+  return (
+    <div className="note new">
+      <textarea
+        onChange={(e) => {
+          handleNoteText(e.target.value);
+        }}
+        value={noteText}
+        cols="8"
+        rows="7"
+        placeholder="Type something here..."
+        maxLength="500"
+      ></textarea>
+      <div className="note-footer">
+        <small> Remaining: {500 - noteText.length} </small>
+        <button onClick={handleAddNote} className="save">
+          Save
+        </button>
+        
+      </div>
+    </div>
+  );
+}
---
+++ src/components/NotesList.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import AddNote from "./AddNote";
+import Note from "./Note";
+
+export default function NotesList({
+  myNotes,
+  handleNoteText,
+  handleAddNote,
+  handleDeleteNotes,
+  noteText,
+}) {
+  const handleDownloadNote = () => {
+  
+    const blob = new Blob([noteText], { type: "text/plain" });
+
+  
+    const url = window.URL.createObjectURL(blob);
+
+   
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "note.txt";
+    document.body.appendChild(link);
+    link.click();
+
+  
+    document.body.removeChild(link);
+    window.URL.revokeObjectURL(url);
+  };
+
+  return (
+    <div className="notes-list">
+      <AddNote
+        noteText={noteText}
+        handleAddNote={handleAddNote}
+        handleNoteText={handleNoteText}
+        handleDownloadNote={handleDownloadNote}
+      />
+      {myNotes.map((note) => {
+        return (
+          <Note
+            handleDeleteNotes={handleDeleteNotes}
+            key={note.id}
+            note={note}
+          />
+        );
+      })}
+    </div>
+  );
+}
